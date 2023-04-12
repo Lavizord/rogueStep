782cf6b01b10478bae6dd03b022b00b8882ada88
MiguelCLG <MiguelCLG@hotmail.com>
2023-04-12 22:53:18 +0100

    feat: added steps counter to appbar

--- src/components/Header/__tests__/Header.test.tsx
+++ src/components/Header/__tests__/Header.test.tsx
@@ -1,30 +1,35 @@
-import {render, screen} from '@testing-library/react'
-import Header from '../Header';
+import { render, screen } from "@testing-library/react";
+import Header from "../Header";
 
 describe("Header Test", () => {
-  it('should render appbar', () => 
-  {
-    render(<Header hp={10} gold={10} />)
+  it("should render appbar", () => {
+    render(<Header hp={10} gold={10} steps={0} />);
 
-    const appBar = screen.getByTestId('app-bar-test-id');
+    const appBar = screen.getByTestId("app-bar-test-id");
 
     expect(appBar).toBeInTheDocument();
   });
+
   it.each`
-  gold   | hp  
-  ${100} | ${1}
-  ${21}  | ${75}
-  ${32}  | ${46}
-`('should render $gold gold coins and $hp hp', ({gold, hp}) => 
-  {
-    render(<Header hp={hp} gold={gold} />)
+    gold   | hp    | steps
+    ${100} | ${1}  | ${1}
+    ${21}  | ${75} | ${4}
+    ${32}  | ${46} | ${123}
+  `(
+    "should render $gold gold coins, $hp hp and $steps steps",
+    ({ gold, hp, steps }) => {
+      render(<Header hp={hp} gold={gold} steps={steps} />);
 
-    const goldChip = screen.getByTestId('gold-chip-test-id');
-    const hpChip = screen.getByTestId('hp-chip-test-id');
+      const goldChip = screen.getByTestId("gold-chip-test-id");
+      const hpChip = screen.getByTestId("hp-chip-test-id");
+      const stepsChip = screen.getByTestId("steps-chip-test-id");
 
-    expect(goldChip).toBeInTheDocument();
-    expect(hpChip).toBeInTheDocument();
-    expect(goldChip).toHaveTextContent(gold);
-    expect(hpChip).toHaveTextContent(hp);
-  });
+      expect(goldChip).toBeInTheDocument();
+      expect(hpChip).toBeInTheDocument();
+      expect(stepsChip).toBeInTheDocument();
+      expect(goldChip).toHaveTextContent(gold);
+      expect(hpChip).toHaveTextContent(hp);
+      expect(stepsChip).toHaveTextContent(steps);
+    }
+  );
 });
--- src/devnotes.md
+++ src/devnotes.md
@@ -5,8 +5,8 @@
   1. Limpar os bugs / tech debt. (LAVI)
   2. Terminar de implementar o alerta de morte (com butões de escolha placeholder). (MIGUEL)
   3. Implementar sistema que impeça a história inicial de se repetir (guardar os storyIds e excluir dee novos). (LAVI)
-  4. Implementar UI na appbar com steps. (MIGUEL)
-  5. Fazer uma validação para apenas aparecerem as notificações que mudam valores (excluir notificações de 0). (MIGUEL)
+  4. ~~Implementar UI na appbar com steps. (MIGUEL)~~
+  5. ~~Fazer uma validação para apenas aparecerem as notificações que mudam valores (excluir notificações de 0). (MIGUEL)~~
      ?. Por último deve ser implementado routing. (MIGUEL)
 
   A EVITAR até os pontos acima serem concluidos.
