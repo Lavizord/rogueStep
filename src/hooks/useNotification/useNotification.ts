d5a35c871050b275542abcd960b64702386b314f
MiguelCLG <MiguelCLG@hotmail.com>
2023-04-13 02:33:31 +0100

    bug: updated position of notifications to be above the button

--- src/hooks/useNotification/useNotification.ts
+++ src/hooks/useNotification/useNotification.ts
@@ -24,11 +24,11 @@ const useNotification = () => {
   const sendNotification = ({ textToShow, style }: EnqueueProps) => {
     enqueueSnackbar(`${textToShow}`, {
       autoHideDuration: 1500,
-      anchorOrigin: { vertical: "bottom", horizontal: "left" },
       style: {
         ...baseStyle,
         ...style,
       },
+      SnackbarProps: { style: { ...baseStyle, border: "none" } },
     });
   };
 
